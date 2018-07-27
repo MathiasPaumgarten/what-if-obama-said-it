4c2e9019280dea54f11a25c53c71a58df10df084
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-07-26 23:18:09 -0700

    CRON updates and basic web page

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,57 @@
+import * as express from "express";
+import { merge } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+
 import { Tracker } from "./tracker";
 
-const cnn = new Tracker( "cnn", 30 );
+const handles = [
+    "CNNPolitics",
+    "voxdotcom",
+    "politico",
+    "BBCWorld",
+    "nprpolitics",
+    "MSNBC",
+    "nytimes",
+    "nytpolitics",
+    "CBSNews",
+    "postpolitics",
+    "thehill",
+    "ABCPolitics",
+    "TheEconomist",
+    "WSJ",
+    "Reuters",
+    "AP",
+];
 
-cnn.change.subscribe( () => {
-    console.log( cnn.getLines().join( "\n" ) );
+const FIVE_HOURS = 1000 * 60 * 60 * 5;
+const trackers = handles.map( handle => new Tracker( handle, 30 ) );
+let lines: string[] = [];
+
+merge( ...trackers.map( tracker => tracker.change ) ).pipe( debounceTime( 1000 ) ).subscribe( refreshList );
+
+function refreshList() {
+    lines = [].concat( ...trackers.map( tracker => tracker.getLines() ) );
+}
+
+function updateTrackers() {
+    trackers.forEach( tracker => tracker.update() );
+
+    setTimeout( updateTrackers, FIVE_HOURS );
+}
+
+setTimeout( updateTrackers, FIVE_HOURS );
+
+// ----------------------------------------
+
+const app = express();
+
+app.set( "view engine", "pug" );
+app.set( "views", __dirname + "/../views" );
+
+app.get( "/", ( _, response ) => {
+
+    response.render( "index", { lines } );
+
 } );
+
+app.listen( process.env.PORT || 1234 );
