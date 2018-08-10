8f08b744b2a504f31b6cf724bf7c1db9e124c17c
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-08-09 22:49:56 -0700

    blank target for links

--- source/client/components/link.tsx
+++ source/client/components/link.tsx
@@ -8,9 +8,9 @@ export class Link extends React.Component<LinkProps, {}> {
     render() {
         return (
             <span className="link">
-                <a href={ this.props.href }>{ this.props.href }</a>
+                <a href={ this.props.href } target="_blank">{ this.props.href }</a>
                 <div className="cover-container">
-                    <a href={ this.props.href }>{ this.props.href }</a>
+                    <a href={ this.props.href } target="_blank">{ this.props.href }</a>
                 </div>
             </span>
         );
